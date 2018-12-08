503f6de97a60ea6b9da777ff77250bd16fd3dcca
Dmitry Kargashin <dkargashin3@gmail.com>
2018-12-09 02:40:53 +0300

    ci

--- pkg/daemon/daemon_test.go
+++ pkg/daemon/daemon_test.go
@@ -5,6 +5,7 @@
 package daemon_test
 
 import (
+	"database/sql"
 	"testing"
 
 	"bmstu.codes/developers34/SBWeb/pkg/api"
@@ -31,6 +32,38 @@ func TestRunService(t *testing.T) {
 			IdleTimeout:  "10s",
 		},
 	}
+
+	db, _ := sql.Open("postgres", "postgresql://runner:@postgres/data?sslmode=disable")
+	db.Exec(`
+	CREATE TABLE IF NOT EXISTS users
+(
+    id                SERIAL      PRIMARY KEY,
+    first_name        varchar(80) NOT NULL,
+    last_name         varchar(80) NOT NULL,
+    email             varchar(80) UNIQUE NOT NULL,
+    password_hash     text        NOT NULL,
+    telephone         varchar(80),
+    about             text,
+    avatar_address    text,
+    reg_time          timestamp   DEFAULT CURRENT_TIMESTAMP NOT NULL
+);`)
+	db.Exec(`
+	CREATE TABLE IF NOT EXISTS ads
+(
+    id             SERIAL       PRIMARY KEY,
+    title          varchar(80)  NOT NULL,
+    price          integer      CONSTRAINT positive_price CHECK (price > 0),
+    country        varchar(80),
+    city           varchar(80),
+    subway_station varchar(80),
+    ad_images      varchar(256)[],
+    -- when deleting user we should delete his ads
+    owner_ad       integer      REFERENCES users (id) ON DELETE CASCADE NOT NULL,
+    description_ad text,
+    creation_time  timestamp    DEFAULT CURRENT_TIMESTAMP NOT NULL
+);`)
+
+	db.Close()
 
 	err := daemon.RunService(cfg)
 	if err != nil {
